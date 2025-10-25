39c75001711b1e1281133b19fd5a60503c3c2dd6
Peter Torki <pjturki@gmail.com>
2025-10-25 11:49:23 +0300

    fix: update for types to import all entities types from index.ts

---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+import { Customer } from "./customerTypes";
+import { Category } from "./categoryTypes";
+import { Item } from "./itemTypes";
+import { Invoice } from "./invoiceTypes";
+import { InvoiceItem, 
+InvoiceDetail,
+
+ } from "./invoiceTypes";
+export { Customer, Category, Item, Invoice, InvoiceItem, InvoiceDetail };
