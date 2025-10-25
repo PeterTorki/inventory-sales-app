f5b0fa5e678d037fc83ce298e9df1ce7d5fa6ff6
Peter Torki <pjturki@gmail.com>
2025-10-25 22:39:46 +0300

    feat, fix: edits and add general settings at items

---
+++ src/screens/items/index.ts
@@ -0,0 +1,5 @@
+// screens/items/index.ts
+export { default as CategoryScreen } from "./CategoryScreen/CategoryScreen";
+export { default as InventoryScreen } from "./InventoryScreen/InventoryScreen";
+export { default as ItemFormScreen } from "./ItemFormScreen/ItemFormScreen";
+export { default as ItemListScreen } from "./ItemListScreen/ItemListScreen";
